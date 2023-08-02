fb951152cc7743004186b54bc0facd9933dce603
Dennis Jabs <dennisjabs1@outlook.de>
2023-08-02 19:41:10 +0200

    Added TicTacToeEnv in gym format (2)

---
+++ classic_games/tictactoe/game/__init__.py
@@ -0,0 +1,7 @@
+import gymnasium as gym
+
+# Register the environment
+gym.register(
+    id='tictactoe-v0',  # Unique identifier for the environment
+    entry_point='classic_games.tictactoe.game.env:TicTacToeEnv',  # Path to the environment class
+)
