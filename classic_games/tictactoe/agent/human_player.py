e2e9f4f172bcd94555ee1c6d0f7b61e6bed6d12a
Dennis Jabs <dennisjabs1@outlook.de>
2023-09-06 13:13:08 +0200

    D. Jabs: - Added human player to the list of players

--- classic_games/tictactoe/agent/human_player.py
+++ classic_games/tictactoe/agent/human_player.py
@@ -2,6 +2,7 @@ from typing import Optional
 from gymnasium.core import ObsType
 
 from classic_games.tictactoe.agent.abstract_player import Player
+from classic_games.tictactoe.model.board import TicTacToeBoard
 
 
 class HumanPlayer(Player):
@@ -17,17 +18,58 @@ class HumanPlayer(Player):
         super().__init__(your_symbol, enemy_symbol, tiles_to_win, player_name, seed)
 
     def start(self, board: ObsType) -> int:
-        # TODO: Implement here
-        pass
+        # Update turns
+        self._turn += 1
+
+        state = TicTacToeBoard(board)
+        actions = state.get_actions()
+
+        if not actions:
+            # Case: There is no valid actions to take anymore
+            return None
+
+        # Wait for human to type a number between 0-8
+        while True:
+            user_input = input(f"Please enter a number from {actions}: ")
+            try:
+                action = int(user_input)
+                if action in actions:
+                    print("You entered a valid number: ", action)
+                    break
+                else:
+                    print("You entered an invalid number: ", action)
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+        return action
 
     def act(self, board: ObsType) -> int:
         # TODO: Implement here
         if self._turn == 0:
             return self.start(board)
         else:
-            pass
+            # Update turns
+            self._turn += 1
+
+            state = TicTacToeBoard(board)
+            actions = state.get_actions()
+
+            if not actions:
+                # Case: There is no valid actions to take anymore
+                return None
+
+            # Wait for human to type a number between 0-8
+            while True:
+                user_input = input(f"Please enter a number from {actions}: ")
+                try:
+                    action = int(user_input)
+                    if action in actions:
+                        print("You entered a valid number: ", action)
+                        break
+                    else:
+                        print("You entered an invalid number: ", action)
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+            return action
 
     def end(self, board: ObsType) -> int:
-        # TODO: Implement here
         pass
-
